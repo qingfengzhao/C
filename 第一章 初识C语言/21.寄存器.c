56a63678de47f16abccd0e4223b8684f42113c73
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-01 21:42:02 +0800

    21.寄存器.c

---
+++ 第一章 初识C语言/21.寄存器.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//寄存器
+//高速缓存
+//内存
+//硬盘
+int main()
+{
+	//register int a=10;//建议把a定义成寄存器变量
+	int a = 20;//int定义的变量是由符号的。signed int=int unsigned int无符号
+	//typeof 类型定义 类型重定义
+	//typeof unsigned int u_int;
+}
