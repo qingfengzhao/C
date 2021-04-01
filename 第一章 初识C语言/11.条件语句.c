6abe39ba5750e2854e4cad3d51106da5b7eebf41
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-01 21:34:36 +0800

    11.条件语句.c

---
+++ 第一章 初识C语言/11.条件语句.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() 
+{ 
+	int coding = 0;
+	printf("你会去敲代码吗？（选择1 or 0）:>");
+	scanf("%d", &coding);
+	if(coding == 1)
+	{
+		printf("坚持，你会有好offer\n");
+	} 
+	else
+	{
+		printf("放弃，回家卖红薯\n");
+	} 
+	return 0; 
+}
