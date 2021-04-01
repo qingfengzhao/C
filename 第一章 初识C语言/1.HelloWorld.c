edb6c0a02628084f814c91dd1d3c5311af9261ef
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-01 20:51:50 +0800

    1.HelloWorld.c
    第一个C语言程序

---
+++ 第一章 初识C语言/1.HelloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()	//主函数，程序的入口，有且仅有一个main函数
+//int 是整型的意思
+//main前面的int表示main函数调用返回一个整型值
+{
+	//控制台输出helloworld
+	//函数-print function-printf-打印函数
+	//库函数-C语言本身提供给我们使用的函数
+	//#inclue <stdio.h> 包含一个叫stdio.h的文件 std-标准 standard input output
+	printf("HelloWorld\n");
+	return 0;//返回0
+}
+
+//还有一种写法,已经过时 
+/*void main()
+{
+}*/
