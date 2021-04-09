62309de34a9794224fc65174524b5e3e3fdce22b
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-09 21:00:42 +0800

    1-1.if语句_悬空else.c

---
+++ 第二章 分支和循环/1-1.if语句_悬空else.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main() {
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("呵呵\n");
+		else
+			printf("哈哈\n");
+	return 0;
+}
