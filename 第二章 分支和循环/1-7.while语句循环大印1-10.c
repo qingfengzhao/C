b5165565835aa868f58efbfa34fe83eeb8b32fe7
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-09 21:06:54 +0800

    1-7.while语句循环大印1-10.c

---
+++ 第二章 分支和循环/1-7.while语句循环大印1-10.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+	//while (i <= 10)
+	//{
+	//	if (i == 5)
+	//		break;
+	//	printf("%d\n", i);
+	//	i++;
+	//}
+
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;
+		printf("%d\n", i);
+		i++;
+	}
+
+
+	return 0;
+}
