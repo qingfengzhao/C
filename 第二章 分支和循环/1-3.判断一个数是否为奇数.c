c27e16c92e857cf8111945291ac48dc6423f9336
qingfengzhao <69064737+qingfengzhao@users.noreply.github.com>
2021-04-09 21:02:23 +0800

    1-3.判断一个数是否为奇数.c

---
+++ 第二章 分支和循环/1-3.判断一个数是否为奇数.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//找出0-100之间的奇数
+
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+
+	//方法1
+	//while (i<=100)
+	//{
+	//	if (i % 2 == 1) {
+	//		printf("%d是奇数\n", i);
+	//	}
+	//	i++;
+	//}
+
+	//方法2
+	//while (i <= 100)
+	//{
+	//	printf("%d\n", i);
+	//	i += 2;
+	//}
+
+
+	return 0;
+}
